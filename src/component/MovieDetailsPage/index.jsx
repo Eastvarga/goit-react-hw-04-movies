3d20e755a1c88c41108380e899cc661a0115e954
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-10 22:36:22 +0300

    add api to MovieDetailsPage

--- src/component/MovieDetailsPage/index.jsx
+++ src/component/MovieDetailsPage/index.jsx
@@ -1,26 +1,64 @@
 import { Component } from "react";
-import Reviews from "../Reviews";
-import Cast from "../Cast";
+import { Link } from "react-router-dom";
+import ApiService from "../services/apiServices";
 class MovieDetails extends Component {
+  state = {
+    movieDetails: null,
+  };
+  componentDidMount() {
+    //   console.log(
+    //     "movieDetails componentDidMount",
+    //     this.props.match.params.movieId
+    //   );
+    ApiService.fetchMovieDetails(this.props.match.params.movieId).then(
+      (data) => {
+        // console.log("data movieDetails", data);
+        this.setState({ movieDetails: data });
+      }
+    );
+  }
   render() {
+    // console.log("MovieDetails render this.state", this.state);
+    // console.log("MovieDetails props", this.props);
+    // const { title, vote_average, overview, genres } = this.state.movieDetails;
     return (
       <div>
         <button type="button">Go back</button>
-        <div>
-          <img src="" alt="" />
+        {this.state.movieDetails && (
           <div>
-            <h2>Film Name</h2>
-            <p>User Score: some score</p>
-            <h3>Overview</h3>
-            <p>Some overview</p>
-            <h3>Genres</h3>
-            <p>array of ganres</p>
+            <img
+              width="400"
+              src={`https://image.tmdb.org/t/p/w780${this.state.movieDetails.backdrop_path}`}
+              alt=""
+            />
+            <div>
+              <h2>{this.state.movieDetails.title}</h2>
+              <p>
+                User Score: {Number(this.state.movieDetails.vote_average) * 10}%
+              </p>
+              <h3>Overview</h3>
+              <p>{this.state.movieDetails.overview}</p>
+              <h3>Genres</h3>
+              <p>
+                {this.state.movieDetails.genres.reduce(
+                  (acc, { name }) => acc + " " + name,
+                  ""
+                )}
+              </p>
+            </div>
           </div>
-        </div>
+        )}
+
         <div>
           <p>Additional information</p>
-          <Cast />
-          <Reviews />
+          <ul>
+            <li>
+              <Link to={`${this.props.match.url}/cast`}>Cast</Link>
+            </li>
+            <li>
+              <Link to={`${this.props.match.url}/reviews`}>Reviews</Link>
+            </li>
+          </ul>
         </div>
       </div>
     );
