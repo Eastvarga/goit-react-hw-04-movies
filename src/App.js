f109f4267eef398d4f091064023a5de1e314beef
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-10 22:35:32 +0300

    update routes on App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavView from "./component/views/NavView";
 import HomeView from "./component/views/HomeView";
 import MoviesView from "./component/views/MoviesView";
+import MovieDetailsView from "./component/views/MovieDetailsView";
+
 import "modern-normalize/modern-normalize.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route component={NavView} />
-
-        <Route exact path="/" component={HomeView} />
-        <Route exact path="/movies" component={MoviesView} />
+        <Route path="/" component={NavView} />
+        <Switch>
+          <Route exact path="/" component={HomeView} />
+          <Route exact path="/movies" component={MoviesView} />
+          <Route path="/movies/:movieId" component={MovieDetailsView} />
+        </Switch>
       </div>
     );
   }
