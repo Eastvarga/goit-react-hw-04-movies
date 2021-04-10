6b5418cfa1c018eaa5acddf5882a33037beaf922
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-10 22:39:10 +0300

    Update MoviesView.js

--- src/component/views/MoviesView.js
+++ src/component/views/MoviesView.js
@@ -1,5 +1,11 @@
 import MoviesPage from "../MoviesPage";
-const MoviesView = () => {
-  return <MoviesPage />;
-};
+import { Component } from "react";
+
+class MoviesView extends Component {
+  render() {
+    // console.log("MoviesView props", this.props);
+    return <MoviesPage {...this.props} />;
+  }
+}
+
 export default MoviesView;
