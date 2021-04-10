b52d85ac7e8d64ac39ec9aa8595eef944a0dadfc
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-10 22:39:49 +0300

    add api to Cast

--- src/component/Cast/index.jsx
+++ src/component/Cast/index.jsx
@@ -1,14 +1,37 @@
 import { Component } from "react";
-
+import ApiService from "../services/apiServices";
 class Cast extends Component {
+  state = {
+    cast: [],
+  };
+  componentDidMount() {
+    ApiService.fetchMovieDetails(this.props.match.params.movieId, "cast").then(
+      ({ cast }) => {
+        // console.log("Cast fetch data", cast);
+        this.setState({ cast: cast });
+      }
+    );
+  }
   render() {
+    // console.log("Cast props", this.props);
     return (
       <ul>
-        <li>
-          <img src="" alt="" />
-          <p>Actor Name</p>
-          <p>Character: some haracter</p>
-        </li>
+        {this.state.cast.length > 0 &&
+          this.state.cast.map(
+            ({ id, profile_path, original_name, character }) => {
+              return (
+                <li key={id}>
+                  <img
+                    width="120"
+                    src={`https://image.tmdb.org/t/p/w154${profile_path}`}
+                    alt=""
+                  />
+                  <p>{original_name}</p>
+                  <p>Character: {character}</p>
+                </li>
+              );
+            }
+          )}
       </ul>
     );
   }
