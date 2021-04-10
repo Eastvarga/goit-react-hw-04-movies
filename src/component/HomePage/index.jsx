4158ab054b175e589660f115732a2efd23162ee5
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-10 22:35:55 +0300

    add api to Hompage

--- src/component/HomePage/index.jsx
+++ src/component/HomePage/index.jsx
@@ -1,22 +1,26 @@
 import { Component } from "react";
-import ApiService from "../services/apiSerivces";
+import { Link } from "react-router-dom";
+import ApiService from "../services/apiServices";
 class HomePage extends Component {
   state = {
     movies: [],
   };
   componentDidMount() {
     ApiService.fetchTrendigMovies().then(({ results }) => {
-      console.log(results);
+      //   console.log(results);
       this.setState({ movies: results });
     });
   }
   render() {
     const movies = this.state.movies;
+    // console.log("HomePage props", this.props);
     return (
       <ul>
         {movies.length > 0 &&
           movies.map(({ original_title, id }) => (
-            <li key={id}>{original_title}</li>
+            <li key={id}>
+              <Link to={`/movies/${id}`}>{original_title}</Link>
+            </li>
           ))}
       </ul>
     );
