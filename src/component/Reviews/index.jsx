b35fa7d389a33e2b09b608edcfe5f5225ce97cec
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-10 16:23:03 +0300

    create Reviews template

---
+++ src/component/Reviews/index.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Rewiews extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <h3>Autor name</h3>
+          <p>Autors content</p>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default Rewiews;
