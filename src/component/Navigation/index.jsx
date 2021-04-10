3db3d34e0fd4e3bbf801ec9d012cfd62d7cd6c66
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-10 22:37:22 +0300

    add links to navigation

--- src/component/Navigation/index.jsx
+++ src/component/Navigation/index.jsx
@@ -1,11 +1,34 @@
 import { Component } from "react";
-import { navigation } from "./styles.module.css";
+import { NavLink } from "react-router-dom";
+import {
+  navigation,
+  navigationLink,
+  navigationLinkActive,
+} from "./styles.module.css";
+
 class Navigation extends Component {
   render() {
     return (
       <ul className={navigation}>
-        <li>Home Link</li>
-        <li>Movies link</li>
+        <li>
+          <NavLink
+            exact
+            to="/"
+            className={navigationLink}
+            activeClassName={navigationLinkActive}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/movies"
+            className={navigationLink}
+            activeClassName={navigationLinkActive}
+          >
+            Movies
+          </NavLink>
+        </li>
       </ul>
     );
   }
