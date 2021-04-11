def43b870a1ff918737a75b01fc35b1488ea5e12
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-11 20:03:45 +0300

    Create AppBar

---
+++ src/component/AppBar/index.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Navigation from "../Navigation";
+
+class AppBar extends Component {
+  render() {
+    return (
+      <header>
+        <Navigation />
+      </header>
+    );
+  }
+}
+
+export default AppBar;
