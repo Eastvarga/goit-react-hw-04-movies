54da5951193eeb7a37de2ae40f304d8b5e65510c
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-11 20:04:08 +0300

    Correct App with routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import NavView from "./component/views/NavView";
+import AppBar from "./component/AppBar";
 import HomeView from "./component/views/HomeView";
 import MoviesView from "./component/views/MoviesView";
 import MovieDetailsView from "./component/views/MovieDetailsView";
-
+import routes from "./routes";
 import "modern-normalize/modern-normalize.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={NavView} />
+        <AppBar />
         <Switch>
-          <Route exact path="/" component={HomeView} />
-          <Route exact path="/movies" component={MoviesView} />
-          <Route path="/movies/:movieId" component={MovieDetailsView} />
+          <Route exact path={routes.home} component={HomeView} />
+          <Route exact path={routes.movies} component={MoviesView} />
+          <Route path={routes.movieDetails} component={MovieDetailsView} />
         </Switch>
       </div>
     );
