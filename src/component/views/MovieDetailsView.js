eab74e5fc8e7a58343012991a605b1abe24790ff
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-11 19:59:05 +0300

    correction MoviesDetails

--- src/component/views/MovieDetailsView.js
+++ src/component/views/MovieDetailsView.js
@@ -1,21 +1,27 @@
 import MovieDetailsPage from "../MovieDetailsPage";
-import Reviews from "../Reviews";
-import Cast from "../Cast";
+// import Reviews from "../Reviews";
+// import Cast from "../Cast";
 import { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+// import { Link, Route, Switch } from "react-router-dom";
+
 class MovieDetailsView extends Component {
   render() {
     // console.log("MovieDetailsView props", this.props);
     return (
       <>
-        <MovieDetailsPage {...this.props} />
-        <Switch>
-          <Route path={`${this.props.match.path}/cast`} component={Cast} />
-          <Route
-            path={`${this.props.match.path}/reviews`}
-            component={Reviews}
-          />
-        </Switch>
+        <MovieDetailsPage />
+        {/* <div>
+          <p>Additional information</p>
+          <ul>
+            <li>
+              <Link to={`${this.props.match.url}/cast`}>Cast</Link>
+            </li>
+            <li>
+              <Link to={`${this.props.match.url}/reviews`}>Reviews</Link>
+            </li>
+          </ul>
+        </div> */}
+       
       </>
     );
   }
