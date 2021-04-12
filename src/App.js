511b5b55571e49ea41be83dfc07b2b6a14f915e8
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-12 15:57:13 +0300

    remove notusable Viewes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import AppBar from "./component/AppBar";
-import HomeView from "./component/views/HomeView";
-import MoviesView from "./component/views/MoviesView";
-import MovieDetailsView from "./component/views/MovieDetailsView";
+import HomePage from "./component/HomePage";
+import MoviesPage from "./component/MoviesPage";
+import MovieDetailsPage from "./component/MovieDetailsPage";
+// import HomeView from "./component/views/HomeView";
+// import MoviesView from "./component/views/MoviesView";
+// import MovieDetailsView from "./component/views/MovieDetailsView";
 import routes from "./routes";
 import "modern-normalize/modern-normalize.css";
 
@@ -13,9 +16,9 @@ class App extends Component {
       <div className="App">
         <AppBar />
         <Switch>
-          <Route exact path={routes.home} component={HomeView} />
-          <Route exact path={routes.movies} component={MoviesView} />
-          <Route path={routes.movieDetails} component={MovieDetailsView} />
+          <Route exact path={routes.home} component={HomePage} />
+          <Route exact path={routes.movies} component={MoviesPage} />
+          <Route path={routes.movieDetails} component={MovieDetailsPage} />
         </Switch>
       </div>
     );
