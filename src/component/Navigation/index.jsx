984ff03dead377d696414925973db57dfba4c719
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-12 21:19:31 +0300

    add style to nav

--- src/component/Navigation/index.jsx
+++ src/component/Navigation/index.jsx
@@ -1,28 +1,28 @@
 import { Component } from "react";
 import { NavLink } from "react-router-dom";
-import {
-  navigation,
-  navigationLink,
-  navigationLinkActive,
-} from "./styles.module.css";
+// import {
+//   navigation,
+//   navigationLink,
+//   navigationLinkActive,
+// } from "./styles.module.css";
 import routes from "../../routes";
 
 class Navigation extends Component {
   render() {
     return (
-      <nav className={navigation}>
+      <nav className="nav mb-2 mt-2 shadow-bot">
         <NavLink
           exact
           to={routes.home}
-          className={navigationLink}
-          activeClassName={navigationLinkActive}
+          className="nav-link font-weight-bold  d-inline-flex p-2 mr-2"
+          activeClassName="text-danger"
         >
           Home
         </NavLink>
         <NavLink
           to={routes.movies}
-          className={navigationLink}
-          activeClassName={navigationLinkActive}
+          className="nav-link font-weight-bold d-inline-flex p-2"
+          activeClassName="text-danger"
         >
           Movies
         </NavLink>
