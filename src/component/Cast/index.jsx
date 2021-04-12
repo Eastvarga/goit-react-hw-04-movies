d85ed67eb6981922b68fcca8edb84a37fda0e380
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-12 15:54:47 +0300

    add spinner to Cast

--- src/component/Cast/index.jsx
+++ src/component/Cast/index.jsx
@@ -1,23 +1,33 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import ApiService from "../services/apiServices";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 class Cast extends Component {
   state = {
     cast: [],
+    isLoading: false,
   };
   componentDidMount() {
-    ApiService.fetchMovieDetails(this.props.match.params.movieId, "cast").then(
-      ({ cast }) => {
+    this.toggleSpinner(this.state);
+    ApiService.fetchMovieDetails(this.props.match.params.movieId, "cast")
+      .then(({ cast }) => {
         // console.log("Cast fetch data", cast);
         this.setState({ cast: cast });
-      }
-    );
+      })
+      .finally(() => this.toggleSpinner(this.state));
+  }
+  toggleSpinner({ isLoading }) {
+    this.setState({ isLoading: !isLoading });
   }
   render() {
     // console.log("Cast props", this.props);
     return (
       <ul>
-        {this.state.cast.length > 0 ? (
+        {this.state.cast.length > 0 && this.state.isLoading && (
+          <Loader type="TailSpin" color="#00BFFF" height={40} width={40} />
+        )}
+        {!this.state.isLoading && this.state.cast.length > 0 ? (
           this.state.cast.map(
             ({ id, profile_path, original_name, character }) => {
               return (
