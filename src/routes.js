3243160211d576114802d1d522a74281f4fcf3e4
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-04-14 11:46:12 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,35 @@
-const routes = {
-  home: "/",
-  movies: "/movies",
-  movieDetails: "/movies/:movieId",
-};
+import { lazy } from "react";
+const HomePage = lazy(() =>
+  import("./component/HomePage/index.jsx" /* webpackChunkName: "Home Page" */)
+);
+const MoviesPage = lazy(() =>
+  import(
+    "./component/MoviesPage/index.jsx" /* webpackChunkName: "Movies Page" */
+  )
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    "./component/MovieDetailsPage/index.jsx" /* webpackChunkName: "MovieDetails Page" */
+  )
+);
+
+const routes = [
+  {
+    path: "/",
+    label: "Home",
+    component: HomePage,
+    exact: true,
+  },
+  {
+    path: "/movies",
+    label: "Movies",
+    component: MoviesPage,
+    exact: true,
+  },
+  {
+    path: "/movies/:movieId",
+    label: "movieDetails",
+    component: MovieDetailsPage,
+  },
+];
 export default routes;
